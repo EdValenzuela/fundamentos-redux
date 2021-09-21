043e85e0f727a063e73f3e5a2bbbeff676b8dcd3
EdValenzuela <edvalenzuelap@gmail.com>
2021-09-21 00:29:34 -0300

    ✨ depurando con redux devTools

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 // import Fruits from './Components/Fruits'
-/* import Counter from './Components/Counter'
-import Info from './Components/Info' */
+import Counter from './Components/Counter'
+//import Info from './Components/Info'
 /* import Todos from './Components/Todos/Todos' */
 // import Controller from './Components/Controller'
-import Blog from './Components/Blog'
+//import Blog from './Components/Blog'
 
 import store from './redux/store'
 
@@ -13,12 +13,12 @@ const App = () => {
   return (
     <Provider store={store}>
       <div>
-        {/* <Info />
-        <Counter /> */}
+        {/* <Info />*/}
+        <Counter />
         {/* <Fruits /> */}
         {/* <Todos /> */}
         {/* <Controller /> */}
-        <Blog />
+        {/* <Blog /> */}
       </div>
     </Provider>
   )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { DELETE_TODO } from "./actions/todoActions";
 import rootReducer from './reducers'
 
@@ -84,8 +85,20 @@ store.dispatch(increment()); */
 //store.dispatch()
 //store.subscribe(func)
 
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = composeWithDevTools({
+  name: 'Redux',
+  realtime: true,
+  trace: true,
+  traceLimit: 20
+})
 // Store
 // Almacenamiento de nuestro estado
-const store = createStore(rootReducer, applyMiddleware(confirmDeleteTodo, logger, thunk));
+const store = createStore(
+  rootReducer, 
+  composeEnhancers(
+    applyMiddleware(logger, thunk)
+  )
+);
 
 export default store
